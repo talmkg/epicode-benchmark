a71f5d50b2875d4ba055455ab89543f2f3b5e27f
Tim Afanasiev <soupsoupchannel@gmail.com>
2022-10-19 12:50:07 +0200

    ready for git

--- benchmark/question.js
+++ benchmark/question.js
@@ -50,7 +50,7 @@ startGame = () => {
 getNewQuestion = () => {
   if (availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS) {
     localStorage.setItem("mostRecentScore", score);
-    return window.location.assign("./results.html");
+    return window.location.assign("../results/results.html");
   }
   questionCounter++;
   progressText.innerText = `Question ${questionCounter} of ${MAX_QUESTIONS}`;
