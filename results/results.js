7e4f1740eb1e879334bfeb28ad3db0e17444470a
Courtney Hampton <jaye0888@gmail.com>
2022-10-20 15:40:10 +0200

    Update results.js
    matching number of questions

--- results/results.js
+++ results/results.js
@@ -1,9 +1,9 @@
 //laying out how scores will be calculated
 
 //made up value for now
-let score = 3;
+let score = 4;
 //max Points would also need to be grabbed from questions JS if the questions were to ever increase
-const maxPoints = 4;
+const maxPoints = 6;
 
 const percentCorrect = parseFloat((score / maxPoints) * 100);
 const percentWrong = parseFloat(100 - percentCorrect);
