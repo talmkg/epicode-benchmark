42d09cdf21e548c2c654d128c650df922a4c6742
Tim Afanasiev <soupsoupchannel@gmail.com>
2022-10-21 13:12:38 +0200

    hotfix

--- results/results.js
+++ results/results.js
@@ -5,9 +5,9 @@ These values must be integers.*/
 //laying out how scores will be calculated
 
 //made up value for now
-let score = 36;
-//max Points would also need to be grabbed from questions JS if the questions were to ever increase
-const maxPoints = 40;
+let score = 3;
+//max Points would also need to be grabbed from questions JS ------------- DONE
+const maxPoints = MAX_QUESTIONS;
 
 const percentCorrect = parseFloat((score / maxPoints) * 100);
 const percentWrong = parseFloat(100 - percentCorrect);
